505c3c7fc910289d2a9171f1b08a7401c924e1a5
Atsuroh <o89.yamanouchi.atsuro@gmail.com>
2021-01-13 14:11:51 +0900

    画像機能導入

--- app/models/tmart.rb
+++ app/models/tmart.rb
@@ -1,2 +1,6 @@
 class Tmart < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :product_name, :text, :category_id, :status_id, :store_id, :before, :after, presence: true
 end
