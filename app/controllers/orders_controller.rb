a04271f50341f25c0d6580df49f1308d02774cdf
Atsuroh <o89.yamanouchi.atsuro@gmail.com>
2021-01-29 23:02:31 +0900

    クレカ実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,14 @@ class OrdersController < ApplicationController
     end
   end
   def create
+    @order = Order.new
+    if @order.valid?
+      pay_tmart
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
   private
   def order_params
@@ -15,5 +23,13 @@ class OrdersController < ApplicationController
   end
   def set_order
     @tmart = Tmart.find(params[:tmart_id])
+  end
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @tmart.after_price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
